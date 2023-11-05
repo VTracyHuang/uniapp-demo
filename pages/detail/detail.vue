8b805b86c4b9109b829a72e1030e8bfca13f5fa6
Tracy <861522731@qq.com>
2023-11-05 11:01:43 +0800

    first commit

---
+++ pages/detail/detail.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="detail">
+		<view class="title">{{objData.title}}</view>
+		<view class="content">{{objData.body}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		onLoad() {
+			
+		},
+		methods:{
+			getDetail(){
+				uni.request({
+					url:"http://jsonplaceholder.typicode.com/posts/1",
+					success:res=>{
+						console.log(res)
+						this.objData=res.data
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.detail{
+	padding: 30rpx;
+	.title{
+		font-size:46rpx;
+		color:#000;
+		padding-bottom:20rpx;
+	}
+	.content{
+		font-size:30rpx;
+		color:#666;
+		padding-bottom:60rpx;
+	}
+}
+</style>
